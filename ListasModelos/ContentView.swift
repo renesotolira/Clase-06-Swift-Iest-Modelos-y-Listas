bbd6e4ff66bdc085c8d0b25bdf4bf6d925dd9c1c
René <rene.soto01@iest.edu.mx>
2023-10-09 12:58:55 -0600

    Ejemplo Modelos, Listas y ForEach

--- ListasModelos/ContentView.swift
+++ ListasModelos/ContentView.swift
@@ -8,14 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var gamesViewModel = GamesViewModel().dummyData()
+    
+
+    func addGame(){
+        gamesViewModel.append(Game(id: 11, image: "zelda", name: "Zelda", console: "SWITCH", price: 1280.00)
+        )
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+       
+       /*List +ForEach*/
+        List() {
+            ForEach(gamesViewModel , id: \.self.uuid) { game in
+                CardView(image: game.image, name: game.name, console: game.console, price: String(game.price))
+                    .padding(.trailing)
+                    .background(Color("cardColor"))
+                    .cornerRadius(15)
+                    .padding(4)
+                    .listRowInsets(EdgeInsets())
+                
+            }.onDelete { (indexSet) in
+                self.gamesViewModel.remove(atOffsets: indexSet)
+                //addGame()
+            }
+        }.listStyle(PlainListStyle())
+    
+        
+      /*Ejemplo solo List
+            List(gamesViewModel , id: \.self.uuid) { game in
+                CardView(image: game.image, name: game.name, console: game.console, price: String(game.price))
+                    .padding(.trailing)
+                    .background(Color("cardColor"))
+                    .cornerRadius(15)
+                    .padding(4)
+                    
+                
+            }
+        */
+        
+        /*ForEach únicamente
+      
+        ScrollView(.vertical){
+            VStack(alignment: .center){
+                ForEach(gamesViewModel) { game in
+                   // LazyVStack(){
+                        CardView(image: game.image, name: game.name, console: game.console, price: String(game.price))
+                            .padding(.trailing)
+                            .background(Color("cardColor"))
+                            .cornerRadius(15)
+                            .padding(4)
+                   // }
+                    
+                }
+            }
+           
         }
-        .padding()
+      */
+        
+      
     }
 }
 
